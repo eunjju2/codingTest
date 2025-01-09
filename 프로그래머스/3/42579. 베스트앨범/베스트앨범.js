2c997432148038fb2e88e66c61ca11ad55c5c151
ejju <101979788+eunjju2@users.noreply.github.com>
2025-01-09 22:11:05 +0900

    [level 3] Title: 베스트앨범, Time: 5.29 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,33 @@
+function solution(genres, plays) {
+    let answer = [];
+    let n = genres.length;
+    let obj = {};
+    let cnt = {};
+    
+    for(let i=0; i<n; i++) {
+        if(obj[genres[i]]) {
+            obj[genres[i]].push([i,plays[i]])
+            cnt[genres[i]] += plays[i]
+        }else {
+            obj[genres[i]] = [[i,plays[i]]]
+            cnt[genres[i]] = plays[i]
+        }
+    }
+    
+    for (const key in obj) {
+        obj[key].sort((a,b) => b[1]-a[1])
+    }
+    const sortArr = Object.entries(cnt).sort((a,b) => b[1]-a[1])
+    console.log(sortArr)
+    console.log(obj)
+    for(const item of sortArr) {
+        if(obj[item[0]].length > 1) {
+            answer.push(obj[item[0]][0][0])
+            answer.push(obj[item[0]][1][0])
+        }else {
+            answer.push(obj[item[0]][0][0])
+        }
+    }
+    
+    return answer;
+}
