675d5665447cec137ddd49d4c4c39044be1a2aee
ejju <101979788+eunjju2@users.noreply.github.com>
2025-02-02 17:25:19 +0900

    [Silver III] Title: N과 M (4), Time: 160 ms, Memory: 12024 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const [N, M] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+let answer = [];
+
+function backTrack(arr, idx) {
+  if (arr.length === M) {
+    answer.push(arr.join(" "));
+    return;
+  }
+
+  for (let i = idx; i <= N; i++) {
+    backTrack([...arr, i], i);
+  }
+}
+
+backTrack([], 1);
+console.log(answer.join("\n"));
