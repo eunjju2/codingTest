8e3502e05a42b3ab6b0d8b9ef78f5f62ab36e750
ejju <101979788+eunjju2@users.noreply.github.com>
2025-02-12 12:33:02 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.77 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    if(n === 1) return 1;
+    if(n === 2) return 2;
+    
+    const dp = Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(let i=3; i<=n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) > 1000000007 ? (dp[i-1] + dp[i-2]) % 1000000007 : (dp[i-1] + dp[i-2]);
+    }
+    
+    return dp[n] % 1000000007;
+}
