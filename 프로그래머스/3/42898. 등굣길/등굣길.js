0d4efe14c49f52768bb5d0ae280b9719f4c88615
ejju <101979788+eunjju2@users.noreply.github.com>
2025-02-21 12:19:11 +0900

    [level 3] Title: 등굣길, Time: 3.69 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -12,14 +12,11 @@ function solution(m, n, puddles) {
     
     for(let i=0; i<n; i++) {
         for(let j=0; j<m; j++) {
-            if(dp[i][j] === -1) {
-                dp[i][j] = 0;
-                continue;
-            }
+            if(dp[i][j] === -1) continue;
             
             
-            if(i>0) dp[i][j] = (dp[i][j] + dp[i-1][j]) % 1000000007;
-            if(j>0) dp[i][j] = (dp[i][j] + dp[i][j-1]) % 1000000007;
+            if(i-1 >= 0 && dp[i-1][j] !== -1) dp[i][j] = (dp[i][j] + dp[i-1][j]) % 1000000007;
+            if(j-1 >= 0 && dp[i][j-1] !== -1) dp[i][j] = (dp[i][j] + dp[i][j-1]) % 1000000007;
         }
     }
     
