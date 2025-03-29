b8577ce4c957d98b02eee480df2fc72d0d5ea412
ejju <101979788+eunjju2@users.noreply.github.com>
2025-03-29 14:37:33 +0900

    [level 2] Title: 3 x n 타일링, Time: 33.26 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.js
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.js
@@ -8,11 +8,13 @@ function solution(n) {
     if(n<4) return dp[n];
     
     for(let i=4; i<=n; i+=2) {
-        dp[i] = (dp[i - 2] * 3) % 1000000007;
+        dp[i] = (dp[i-2] * 3);
         
         for(let j=0; j<=i-4; j+=2) {
-            dp[i] = (dp[i] + (dp[j]*2) % 1000000007) % 1000000007;
+            dp[i] += dp[j]*2;
         }
+        
+        dp[i] %= 1000000007;
     }
 
 
